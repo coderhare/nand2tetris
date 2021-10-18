13f79629234bc75975bd2a3eeaa8b8b6e2b62fb4
MoriSummer <54202741+coderhare@users.noreply.github.com>
2021-10-18 19:45:25 +0800

    Delete Documents/Code directory

--- Documents/Code/Assembler_Dev/cmake-build-debug/CMakeFiles/Assembler_Dev_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Assembler_Dev_autogen/mocs_compilation.cpp"
-  "CMakeFiles/Assembler_Dev_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Assembler_Dev_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
